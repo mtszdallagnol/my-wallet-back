f75cf65366155862c214293dadd437ac4719c82a
= <=>
2025-03-24 17:11:39 -0700

    DB connection pool

---
+++ src/UserHandler.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+
+public class UserHandler {
+    private Connection conn;
+
+    UserHandler(Connection conn) {
+        this.conn = conn;
+    }
+
+    private class User {
+        
+    }
+}
