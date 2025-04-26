c2e7e343a6d209807c48ad310aa356712074ea5f
gkaiserr <guilherme.kai100@gmail.com>
2025-04-26 20:35:58 -0300

    Wallet DELETE. Code Review

--- src/Wallets/WalletDTO.java
+++ src/Wallets/WalletDTO.java
@@ -19,4 +19,20 @@ public class WalletDTO {
         @MaxLength(200)
         public String descricao;
     }
+
+    @Table(TableName = "carteiras")
+    public static class updateRequirementModel {
+        @Required
+        public Integer id;
+
+        @Required
+        public Integer id_usuario;
+
+        @Unique(withFields = {"id_usuario"}, message = "Carteira com mesmo nome já criada pelo usuário")
+        @MaxLength(50)
+        public String nome;
+
+        @MaxLength(200)
+        public String descricao;
+    }
 }
